516c6efe0f763f3d9b4f023cb813db35125e0063
罗泽轩 <spacewanderlzx@gmail.com>
2023-11-02 11:19:11 +0800

    improve integration tests (#5)
    Signed-off-by: spacewander <spacewanderlzx@gmail.com>

--- tests/integration/plugins/data_plane/data_plane.go
+++ tests/integration/plugins/data_plane/data_plane.go
@@ -2,7 +2,6 @@ package data_plane
 
 import (
 	"context"
-	"io"
 	"net"
 	"net/http"
 	"os"
@@ -133,11 +132,16 @@ func (dp *DataPlane) Stop() {
 	f.Close()
 }
 
-func (dp *DataPlane) Do(method string, path string, body io.Reader) (*http.Response, error) {
-	req, err := http.NewRequest(method, "http://localhost:10000"+path, body)
+func (dp *DataPlane) Get(path string, header http.Header) (*http.Response, error) {
+	req, err := http.NewRequest("GET", "http://localhost:10000"+path, nil)
 	if err != nil {
 		return nil, err
 	}
+	req.Header = header
+	return dp.do(req)
+}
+
+func (dp *DataPlane) do(req *http.Request) (*http.Response, error) {
 	tr := &http.Transport{DialContext: func(ctx context.Context, proto, addr string) (conn net.Conn, err error) {
 		return net.DialTimeout("tcp", ":10000", 1*time.Second)
 	}}
