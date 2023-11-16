2f22a49b877d5f3df21187deef53c16dae623bb0
罗泽轩 <spacewanderlzx@gmail.com>
2023-11-16 19:43:18 +0800

    filter manager: part 5, add doc (#37)
    Fix https://github.com/mosn/moe/issues/22
    Signed-off-by: spacewander <spacewanderlzx@gmail.com>
    
    ---------
    
    Signed-off-by: spacewander <spacewanderlzx@gmail.com>

--- pkg/filtermanager/api/result_action.go
+++ pkg/filtermanager/api/result_action.go
@@ -2,6 +2,7 @@ package api
 
 import "net/http"
 
+// ResultAction is the result returned by each Filter method
 type ResultAction interface {
 	OK()
 }
@@ -9,15 +10,25 @@ type isResultAction struct {
 }
 
 var (
+	// Continue is a placeholder which indicates the process can continue without steering
 	Continue ResultAction = nil
 )
 
 func (i *isResultAction) OK() {}
 
+// LocalResponse represents the reply sent directly to the client instead of using the
+// upstream response. Return `&LocalResponse{Code: 4xx, ...}` in the method if you want
+// to send such a reply.
 type LocalResponse struct {
 	isResultAction
 
-	Code   int
+	Code int
+	// If the Msg is not empty, we will set the reply's body according to the Msg.
+	// The rule to generate body is:
+	// 1. If Content-Type is specified in the Header, the Msg will be sent directly.
+	// 2. If the response header is received, and the Content-Type is "application/json", the Msg is wrapped into a JSON like `{"msg": $MSG}`.
+	// 3. If the request doesn't have Content-Type or the Content-Type is "application/json", the Msg is wrapped into a JSON.
+	// 4. Otherwise, the Msg will be sent directly.
 	Msg    string
 	Header http.Header
 }
