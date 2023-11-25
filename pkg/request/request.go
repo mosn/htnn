d64d5af2404da06dd6c9c9c5a84a089db1986839
罗泽轩 <spacewanderlzx@gmail.com>
2023-11-25 14:09:06 +0800

    satisfy nilaway (#58)

--- pkg/request/request.go
+++ pkg/request/request.go
@@ -1,6 +1,7 @@
 package request
 
 import (
+	"fmt"
 	"net/url"
 
 	"github.com/envoyproxy/envoy/contrib/golang/common/go/api"
@@ -10,7 +11,7 @@ func GetUrl(header api.RequestHeaderMap) *url.URL {
 	path := header.Path()
 	uri, err := url.ParseRequestURI(path)
 	if err != nil {
-		api.LogErrorf("failed to parse uri: %v", err)
+		panic(fmt.Sprintf("unexpected bad request uri given by envoy: %v", err))
 	}
 	return uri
 }
