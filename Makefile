edfc9601cba28262c7f5bbf3bc695f8f1cc6a31b
spacewander <spacewanderlzx@gmail.com>
2023-12-07 16:41:49 +0800

    controller: unite log library
    Signed-off-by: spacewander <spacewanderlzx@gmail.com>

--- Makefile
+++ Makefile
@@ -23,7 +23,7 @@ PROTOC = protoc
 PROTO_FILES = $(call rwildcard,./,*.proto)
 GO_TARGETS = $(patsubst %.proto,%.pb.go,$(PROTO_FILES))
 
-TEST_OPTION ?= -gcflags="all=-N -l" -v -race
+TEST_OPTION ?= -gcflags="all=-N -l" -race
 
 MOUNT_GOMOD_CACHE = -v $(shell go env GOPATH):/go
 ifeq ($(IN_CI), true)
--- controller/cmd/main.go
+++ controller/cmd/main.go
@@ -27,11 +27,11 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 
 	mosniov1 "mosn.io/moe/controller/api/v1"
 	"mosn.io/moe/controller/internal/controller"
+	"mosn.io/moe/pkg/log"
 )
 
 // Version is specified by build tag, in VERSION file
@@ -61,13 +61,9 @@ func main() {
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
-	opts := zap.Options{
-		Development: true,
-	}
-	opts.BindFlags(flag.CommandLine)
 	flag.Parse()
 
-	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+	ctrl.SetLogger(log.DefaultLogger)
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme:                 scheme,
