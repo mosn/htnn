05f96d2f06d6059d6c2bdbbdf1fd088901cdf18e
罗泽轩 <spacewanderlzx@gmail.com>
2024-01-17 15:35:03 +0800

    add consumerRestriction plugin (#220)
    Signed-off-by: spacewander <spacewanderlzx@gmail.com>

---
+++ plugins/consumer_restriction/config.go
@@ -0,0 +1,71 @@
+// Copyright The HTNN Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package consumer_restriction
+
+import (
+	"mosn.io/htnn/pkg/filtermanager/api"
+	"mosn.io/htnn/pkg/plugins"
+)
+
+const (
+	Name = "consumerRestriction"
+)
+
+func init() {
+	plugins.RegisterHttpPlugin(Name, &plugin{})
+}
+
+type plugin struct {
+	plugins.PluginMethodDefaultImpl
+}
+
+func (p *plugin) Type() plugins.PluginType {
+	return plugins.TypeAuthz
+}
+
+func (p *plugin) Order() plugins.PluginOrder {
+	return plugins.PluginOrder{
+		Position: plugins.OrderPositionAuthz,
+	}
+}
+
+func (p *plugin) ConfigFactory() api.FilterConfigFactory {
+	return configFactory
+}
+
+func (p *plugin) Config() api.PluginConfig {
+	return &config{}
+}
+
+type config struct {
+	Config
+
+	allow     bool
+	consumers map[string]struct{}
+}
+
+func (conf *config) Init(cb api.ConfigCallbackHandler) error {
+	rules := conf.GetDeny()
+	if rules == nil {
+		rules = conf.GetAllow()
+		conf.allow = true
+	}
+
+	conf.consumers = make(map[string]struct{}, len(rules.Rules))
+	for _, r := range rules.Rules {
+		conf.consumers[r.Name] = struct{}{}
+	}
+	return nil
+}
---
+++ plugins/tests/integration/consumer_restriction_test.go
@@ -0,0 +1,116 @@
+// Copyright The HTNN Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package integration
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"mosn.io/htnn/pkg/filtermanager"
+	"mosn.io/htnn/pkg/filtermanager/model"
+	"mosn.io/htnn/plugins/tests/integration/control_plane"
+	"mosn.io/htnn/plugins/tests/integration/data_plane"
+)
+
+func TestConsumerRestriction(t *testing.T) {
+	dp, err := data_plane.StartDataPlane(t, nil)
+	if err != nil {
+		t.Fatalf("failed to start data plane: %v", err)
+		return
+	}
+	defer dp.Stop()
+
+	tests := []struct {
+		name   string
+		config *filtermanager.FilterManagerConfig
+		run    func(t *testing.T)
+	}{
+		{
+			name: "allow",
+			config: control_plane.NewPluinConfig([]*model.FilterConfig{
+				{
+					Name: "keyAuth",
+					Config: map[string]interface{}{
+						"keys": []interface{}{
+							map[string]interface{}{
+								"name": "Authorization",
+							},
+						},
+					},
+				},
+				{
+					Name: "consumerRestriction",
+					Config: map[string]interface{}{
+						"allow": map[string]interface{}{
+							"rules": []interface{}{
+								map[string]interface{}{
+									"name": "tom",
+								},
+							},
+						},
+					},
+				},
+			}),
+			run: func(t *testing.T) {
+				resp, _ := dp.Get("/echo", http.Header{"Authorization": []string{"marvin"}})
+				assert.Equal(t, 403, resp.StatusCode)
+				resp, _ = dp.Get("/echo", http.Header{"Authorization": []string{"tom"}})
+				assert.Equal(t, 200, resp.StatusCode)
+			},
+		},
+		{
+			name: "deny",
+			config: control_plane.NewPluinConfig([]*model.FilterConfig{
+				{
+					Name: "keyAuth",
+					Config: map[string]interface{}{
+						"keys": []interface{}{
+							map[string]interface{}{
+								"name": "Authorization",
+							},
+						},
+					},
+				},
+				{
+					Name: "consumerRestriction",
+					Config: map[string]interface{}{
+						"deny": map[string]interface{}{
+							"rules": []interface{}{
+								map[string]interface{}{
+									"name": "tom",
+								},
+							},
+						},
+					},
+				},
+			}),
+			run: func(t *testing.T) {
+				resp, _ := dp.Get("/echo", http.Header{"Authorization": []string{"marvin"}})
+				assert.Equal(t, 200, resp.StatusCode)
+				resp, _ = dp.Get("/echo", http.Header{"Authorization": []string{"tom"}})
+				assert.Equal(t, 403, resp.StatusCode)
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			controlPlane.UseGoPluginConfig(tt.config, dp)
+			tt.run(t)
+		})
+	}
+}
