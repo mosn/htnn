6af30a1d95a4cc04b81156fee5eb5c74e0eae761
罗泽轩 <spacewanderlzx@gmail.com>
2024-01-23 10:12:07 +0800

    docs: add hmac_auth (#241)
    Signed-off-by: spacewander <spacewanderlzx@gmail.com>

--- site/cmd/translator/main.go
+++ site/cmd/translator/main.go
@@ -19,8 +19,12 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"strings"
 	"text/template"
+
+	"github.com/waigani/diffparser"
 
 	"mosn.io/htnn/site/cmd/translator/language"
 )
@@ -30,6 +34,7 @@ var (
 	prompt string
 )
 
+var commit = flag.String("c", "HEAD", "the base commit to compare with when doing incremental translation")
 var inputFile = flag.String("f", "", "the file to translate")
 var fromLocale = flag.String("from", "en", "the locale code that we translate from")
 var toLocale = flag.String("to", "en", "the locale code that we translate to")
@@ -40,10 +45,40 @@ func mustNoErr(err error) {
 	}
 }
 
+type Chunk struct {
+	id    int
+	text  string
+	start int
+	end   int
+}
+
+type Range struct {
+	start int
+	end   int
+}
+
+func findAffectedChunks(ranges []Range, chunks []Chunk) []Chunk {
+	affectedChunks := []Chunk{}
+	i, j := 0, 0
+	for i < len(ranges) && j < len(chunks) {
+		r := ranges[i]
+		c := chunks[j]
+		if r.end < c.start {
+			i++
+		} else if r.start > c.end {
+			j++
+		} else {
+			affectedChunks = append(affectedChunks, c)
+			j++
+		}
+	}
+	return affectedChunks
+}
+
 func main() {
 	flag.Parse()
 
-	if inputFile == nil || *inputFile == "" {
+	if *inputFile == "" {
 		panic("no input file")
 	}
 
@@ -60,7 +95,86 @@ func main() {
 	b, err := os.ReadFile(*inputFile)
 	mustNoErr(err)
 	name := filepath.Base(*inputFile)
-	text := "+++" + name + "\n" + string(b) + "\n+++\n"
+	text := string(b)
+
+	diff := ""
+	outputFile := *inputFile
+	outputFile = strings.Replace(outputFile, strings.ToLower(*fromLocale), strings.ToLower(*toLocale), 1)
+	if _, err := os.Stat(outputFile); err == nil {
+		cmdline := fmt.Sprintf("git diff %s -- %s", *commit, *inputFile)
+		cmd := strings.Fields(cmdline)
+		runDiff := exec.Command(cmd[0], cmd[1:]...)
+		b, err = runDiff.Output()
+		mustNoErr(err)
+
+		diff = string(b)
+	}
+
+	if diff == "" {
+		text = "+++" + name + "\n" + text + "\n+++\n"
+	} else {
+		lines := strings.Split(text, "\n")
+		start := 0
+
+		chunks := []Chunk{}
+		for i, line := range lines {
+			if strings.TrimSpace(line) == "" {
+				chunks = append(chunks, Chunk{
+					id:    len(chunks),
+					text:  strings.Join(lines[start:i], "\n"),
+					start: start,
+					end:   i,
+				})
+				start = i + 1
+			}
+		}
+
+		parsed, _ := diffparser.Parse(diff)
+		hunks := parsed.Files[0].Hunks
+		ranges := []Range{}
+		for _, h := range hunks {
+			n := len(h.NewRange.Lines)
+			startHunk := 0
+			for i := 0; i < n; i++ {
+				li := h.NewRange.Lines[i]
+				if startHunk == 0 {
+					if li.Mode == diffparser.ADDED {
+						startHunk = li.Number
+					}
+				} else if li.Mode != diffparser.ADDED {
+					ranges = append(ranges, Range{
+						start: startHunk,
+						end:   li.Number - 1,
+					})
+					startHunk = 0
+				}
+			}
+
+			if startHunk != 0 {
+				ranges = append(ranges, Range{
+					start: startHunk,
+					end:   h.NewRange.Lines[n-1].Number,
+				})
+			}
+		}
+
+		affectedChunks := findAffectedChunks(ranges, chunks)
+		out := strings.Builder{}
+		// must have at least one affected chunk
+		out.WriteString(affectedChunks[0].text)
+		for i, c := range affectedChunks[1:] {
+			if c.id == affectedChunks[i].id+1 {
+				out.WriteString("\n\n")
+			} else {
+				// this acts as separator
+				out.WriteString("\n...\n")
+			}
+			out.WriteString(c.text)
+		}
+
+		// pretend as a new doc
+		text = fmt.Sprintf("+++%s\n", name) + out.String() + "\n+++\n"
+	}
 
 	t, err := template.New("prompt").Parse(prompt)
 	mustNoErr(err)
