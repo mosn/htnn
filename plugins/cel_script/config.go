3a068fe0fed2befd2721db0f30e25312c4cc1ffa
罗泽轩 <spacewanderlzx@gmail.com>
2024-02-04 10:01:05 +0800

    add celScript plugin (#267)
    Signed-off-by: spacewander <spacewanderlzx@gmail.com>

---
+++ plugins/cel_script/config.go
@@ -0,0 +1,82 @@
+// Copyright The HTNN Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cel_script
+
+import (
+	"github.com/google/cel-go/cel"
+
+	"mosn.io/htnn/pkg/expr"
+	"mosn.io/htnn/pkg/filtermanager/api"
+	"mosn.io/htnn/pkg/plugins"
+)
+
+const (
+	Name = "celScript"
+)
+
+func init() {
+	plugins.RegisterHttpPlugin(Name, &plugin{})
+}
+
+type plugin struct {
+	plugins.PluginMethodDefaultImpl
+}
+
+func (p *plugin) Type() plugins.PluginType {
+	return plugins.TypeTraffic
+}
+
+func (p *plugin) Order() plugins.PluginOrder {
+	return plugins.PluginOrder{
+		Position: plugins.OrderPositionTraffic,
+	}
+}
+
+func (p *plugin) ConfigFactory() api.FilterConfigFactory {
+	return configFactory
+}
+
+func (p *plugin) Config() api.PluginConfig {
+	return &config{}
+}
+
+type config struct {
+	Config
+
+	allowIfScript expr.Script
+}
+
+func (conf *config) Validate() error {
+	err := conf.Config.Validate()
+	if err != nil {
+		return err
+	}
+
+	if conf.AllowIf != "" {
+		_, err = expr.CompileCel(conf.AllowIf, cel.BoolType)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (conf *config) Init(cb api.ConfigCallbackHandler) error {
+	if conf.AllowIf != "" {
+		s, _ := expr.CompileCel(conf.AllowIf, cel.BoolType)
+		conf.allowIfScript = s
+	}
+	return nil
+}
