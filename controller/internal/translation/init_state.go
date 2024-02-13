b9614ecae7d0dca2127156635cbad28a90c1eb92
罗泽轩 <spacewanderlzx@gmail.com>
2024-02-13 13:23:46 +0800

    fix(controller): policy was added repeatedly per gateway (#287)
    Signed-off-by: spacewander <spacewanderlzx@gmail.com>

--- controller/internal/translation/init_state.go
+++ controller/internal/translation/init_state.go
@@ -29,20 +29,19 @@ import (
 type VirtualServicePolicies struct {
 	VirtualService *istiov1b1.VirtualService
 	RoutePolicies  map[string][]*HTTPFilterPolicyWrapper
+	Gateways       []*istiov1b1.Gateway
 }
 
 type HTTPRoutePolicies struct {
 	HTTPRoute     *gwapiv1.HTTPRoute
 	RoutePolicies map[string][]*HTTPFilterPolicyWrapper
+	Gateways      []*gwapiv1.Gateway
 }
 
 // InitState is the beginning of our translation.
 type InitState struct {
 	VirtualServicePolicies map[types.NamespacedName]*VirtualServicePolicies
-	VsToGateway            map[types.NamespacedName][]*istiov1b1.Gateway
-
-	HTTPRoutePolicies map[types.NamespacedName]*HTTPRoutePolicies
-	HrToGateway       map[types.NamespacedName][]*gwapiv1.Gateway
+	HTTPRoutePolicies      map[types.NamespacedName]*HTTPRoutePolicies
 
 	logger *logr.Logger
 }
@@ -50,16 +49,26 @@ type InitState struct {
 func NewInitState(logger *logr.Logger) *InitState {
 	return &InitState{
 		VirtualServicePolicies: make(map[types.NamespacedName]*VirtualServicePolicies),
-		VsToGateway:            make(map[types.NamespacedName][]*istiov1b1.Gateway),
-
-		HTTPRoutePolicies: make(map[types.NamespacedName]*HTTPRoutePolicies),
-		HrToGateway:       make(map[types.NamespacedName][]*gwapiv1.Gateway),
+		HTTPRoutePolicies:      make(map[types.NamespacedName]*HTTPRoutePolicies),
 
 		logger: logger,
 	}
 }
 
-func (s *InitState) AddPolicyForVirtualService(policy *mosniov1.HTTPFilterPolicy, vs *istiov1b1.VirtualService, gw *istiov1b1.Gateway) {
+func (s *InitState) GetGatewaysWithVirtualService(vs *istiov1b1.VirtualService) []*istiov1b1.Gateway {
+	nn := types.NamespacedName{
+		Namespace: vs.Namespace,
+		Name:      vs.Name,
+	}
+
+	vsp, ok := s.VirtualServicePolicies[nn]
+	if !ok {
+		return nil
+	}
+	return vsp.Gateways
+}
+
+func (s *InitState) AddPolicyForVirtualService(policy *mosniov1.HTTPFilterPolicy, vs *istiov1b1.VirtualService, gws []*istiov1b1.Gateway) {
 	nn := types.NamespacedName{
 		Namespace: vs.Namespace,
 		Name:      vs.Name,
@@ -70,6 +79,7 @@ func (s *InitState) AddPolicyForVirtualService(policy *mosniov1.HTTPFilterPolicy
 		vsp = &VirtualServicePolicies{
 			VirtualService: vs,
 			RoutePolicies:  map[string][]*HTTPFilterPolicyWrapper{},
+			Gateways:       gws,
 		}
 		s.VirtualServicePolicies[nn] = vsp
 	}
@@ -89,15 +99,23 @@ func (s *InitState) AddPolicyForVirtualService(policy *mosniov1.HTTPFilterPolicy
 			scope:            PolicyScopeRule,
 		})
 	}
-
-	gws, ok := s.VsToGateway[nn]
-	if !ok {
-		gws = make([]*istiov1b1.Gateway, 0)
-	}
-	s.VsToGateway[nn] = append(gws, gw)
 }
 
-func (s *InitState) AddPolicyForHTTPRoute(policy *mosniov1.HTTPFilterPolicy, route *gwapiv1.HTTPRoute, gw *gwapiv1.Gateway) {
+func (s *InitState) GetGatewaysWithHTTPRoute(route *gwapiv1.HTTPRoute) []*gwapiv1.Gateway {
+	nn := types.NamespacedName{
+		Namespace: route.Namespace,
+		Name:      route.Name,
+	}
+
+	hp, ok := s.HTTPRoutePolicies[nn]
+	if !ok {
+		return nil
+	}
+
+	return hp.Gateways
+}
+
+func (s *InitState) AddPolicyForHTTPRoute(policy *mosniov1.HTTPFilterPolicy, route *gwapiv1.HTTPRoute, gws []*gwapiv1.Gateway) {
 	nn := types.NamespacedName{
 		Namespace: route.Namespace,
 		Name:      route.Name,
@@ -108,6 +126,7 @@ func (s *InitState) AddPolicyForHTTPRoute(policy *mosniov1.HTTPFilterPolicy, rou
 		hp = &HTTPRoutePolicies{
 			HTTPRoute:     route,
 			RoutePolicies: map[string][]*HTTPFilterPolicyWrapper{},
+			Gateways:      gws,
 		}
 		s.HTTPRoutePolicies[nn] = hp
 	}
@@ -119,12 +138,6 @@ func (s *InitState) AddPolicyForHTTPRoute(policy *mosniov1.HTTPFilterPolicy, rou
 			scope:            PolicyScopeRoute,
 		})
 	}
-
-	gws, ok := s.HrToGateway[nn]
-	if !ok {
-		gws = make([]*gwapiv1.Gateway, 0)
-	}
-	s.HrToGateway[nn] = append(gws, gw)
 }
 
 func (s *InitState) Process(original_ctx context.Context) (*FinalState, error) {
