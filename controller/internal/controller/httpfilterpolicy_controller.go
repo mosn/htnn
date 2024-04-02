4df1eb9a6c8a3da94ca69edf15befc37c8fbf5b5
spacewander <spacewanderlzx@gmail.com>
2024-04-02 14:25:48 +0800

    typo
    Signed-off-by: spacewander <spacewanderlzx@gmail.com>

--- controller/internal/controller/httpfilterpolicy_controller.go
+++ controller/internal/controller/httpfilterpolicy_controller.go
@@ -424,7 +424,7 @@ func (r *HTTPFilterPolicyReconciler) policyToTranslationState(ctx context.Contex
 		// We require the embedded policy to be valid, otherwise it's costly to validate and hard to report the error.
 
 		policy.Namespace = vs.Namespace
-		// Name convension is "embedded-$kind-$name"
+		// Name convention is "embedded-$kind-$name"
 		policy.Name = "embedded-virtualservice-" + vs.Name
 		err = r.resolveWithVirtualService(ctx, logger, vs, &policy, initState, istioGwIdx)
 		if err != nil {
