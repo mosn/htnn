ffb5d2582ac65283965df04ee4c43e199a96960d
spacewander <spacewanderlzx@gmail.com>
2024-07-04 12:04:13 +0800

    validate any
    Signed-off-by: spacewander <spacewanderlzx@gmail.com>

--- types/plugins/networkrbac/config.go
+++ types/plugins/networkrbac/config.go
@@ -15,7 +15,12 @@
 package networkrbac
 
 import (
+	"fmt"
+
+	matcher "github.com/cncf/xds/go/xds/type/matcher/v3"
+	rbacconfig "github.com/envoyproxy/go-control-plane/envoy/config/rbac/v3"
 	rbac "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/rbac/v3"
+	"google.golang.org/protobuf/proto"
 
 	"mosn.io/htnn/api/pkg/filtermanager/api"
 	"mosn.io/htnn/api/pkg/plugins"
@@ -43,6 +48,74 @@ func (p *Plugin) Type() plugins.PluginType {
 	return plugins.TypeAuthz
 }
 
+type CustomConfig struct {
+	rbac.RBAC
+}
+
+var matchingNetworkInputs = []string{
+	"type.googleapis.com/envoy.extensions.matching.common_inputs.network.v3.DestinationIPInput", "",
+	"type.googleapis.com/envoy.extensions.matching.common_inputs.network.v3.DestinationPortInput",
+	"type.googleapis.com/envoy.extensions.matching.common_inputs.network.v3.SourceIPInput",
+	"type.googleapis.com/envoy.extensions.matching.common_inputs.network.v3.SourcePortInput",
+	"type.googleapis.com/envoy.extensions.matching.common_inputs.network.v3.DirectSourceIPInput",
+	"type.googleapis.com/envoy.extensions.matching.common_inputs.network.v3.SourceTypeInput",
+	"type.googleapis.com/envoy.extensions.matching.common_inputs.network.v3.ServerNameInput",
+	"type.googleapis.com/envoy.extensions.matching.common_inputs.network.v3.TransportProtocolInput",
+	"type.googleapis.com/envoy.extensions.matching.common_inputs.network.v3.ApplicationProtocolInput",
+	"type.googleapis.com/envoy.extensions.matching.common_inputs.network.v3.FilterStateInput",
+	"type.googleapis.com/envoy.extensions.matching.common_inputs.ssl.v3.UriSanInput",
+	"type.googleapis.com/envoy.extensions.matching.common_inputs.ssl.v3.DnsSanInput",
+	"type.googleapis.com/envoy.extensions.matching.common_inputs.ssl.v3.SubjectInput",
+}
+
+func (conf *CustomConfig) Validate() error {
+	err := conf.RBAC.Validate()
+	if err != nil {
+		return err
+	}
+
+	m := conf.RBAC.GetMatcher().GetMatcherTree()
+	typedURL := m.GetInput().GetTypedConfig().GetTypeUrl()
+	found := false
+	for _, allowList := range matchingNetworkInputs {
+		if typedURL == allowList {
+			found = true
+			break
+		}
+	}
+	if !found {
+		return fmt.Errorf("matcher.matcherTree.input.typedConfig.typeUrl must be one of %v, got %s", matchingNetworkInputs, typedURL)
+	}
+
+	if m.GetCustomMatch().GetTypedConfig() != nil {
+		typedURL = m.GetCustomMatch().GetTypedConfig().GetTypeUrl()
+		if typedURL != "type.googleapis.com/xds.type.matcher.v3.IPMatcher" {
+			return fmt.Errorf("matcher.matcherTree.customMatch.typedConfig.typeUrl must be type.googleapis.com/xds.type.matcher.v3.IPMatcher, got %s", typedURL)
+		}
+		v := m.GetCustomMatch().GetTypedConfig().GetValue()
+		matcher := &matcher.IPMatcher{}
+		// We always call Validate after Unmarshal success
+		_ = proto.Unmarshal(v, matcher)
+		err := matcher.Validate()
+		if err != nil {
+			return err
+		}
+
+		for _, rg := range matcher.GetRangeMatchers() {
+			v = rg.GetOnMatch().GetAction().GetTypedConfig().GetValue()
+			action := &rbacconfig.Action{}
+			_ = proto.Unmarshal(v, action)
+			err := action.Validate()
+			if err != nil {
+				return err
+			}
+		}
+	}
+	// TODO: validate the Action in the other matcher like ExactMatchMap
+	// Another TODO: do it more smartly
+	return nil
+}
+
 func (p *Plugin) Config() api.PluginConfig {
-	return &rbac.RBAC{}
+	return &CustomConfig{}
 }
