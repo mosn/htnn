991654ac9407f2d88ac32515ecd2497e706a7136
罗泽轩 <spacewanderlzx@gmail.com>
2024-07-19 09:57:57 +0800

    mv api module integration test out of test framework (#645)
    Signed-off-by: spacewander <spacewanderlzx@gmail.com>

--- api/tests/integration/config.pb.go
+++ api/tests/integration/config.pb.go
@@ -16,7 +16,7 @@
 // versions:
 // 	protoc-gen-go v1.31.0
 // 	protoc        v4.24.4
-// source: api/plugins/tests/integration/config.proto
+// source: api/tests/integration/config.proto
 
 package integration
 
@@ -51,7 +51,7 @@ type Config struct {
 func (x *Config) Reset() {
 	*x = Config{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_api_plugins_tests_integration_config_proto_msgTypes[0]
+		mi := &file_api_tests_integration_config_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -64,7 +64,7 @@ func (x *Config) String() string {
 func (*Config) ProtoMessage() {}
 
 func (x *Config) ProtoReflect() protoreflect.Message {
-	mi := &file_api_plugins_tests_integration_config_proto_msgTypes[0]
+	mi := &file_api_tests_integration_config_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -77,7 +77,7 @@ func (x *Config) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use Config.ProtoReflect.Descriptor instead.
 func (*Config) Descriptor() ([]byte, []int) {
-	return file_api_plugins_tests_integration_config_proto_rawDescGZIP(), []int{0}
+	return file_api_tests_integration_config_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *Config) GetNeed() bool {
@@ -136,7 +136,7 @@ type BadPluginConfig struct {
 func (x *BadPluginConfig) Reset() {
 	*x = BadPluginConfig{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_api_plugins_tests_integration_config_proto_msgTypes[1]
+		mi := &file_api_tests_integration_config_proto_msgTypes[1]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -149,7 +149,7 @@ func (x *BadPluginConfig) String() string {
 func (*BadPluginConfig) ProtoMessage() {}
 
 func (x *BadPluginConfig) ProtoReflect() protoreflect.Message {
-	mi := &file_api_plugins_tests_integration_config_proto_msgTypes[1]
+	mi := &file_api_tests_integration_config_proto_msgTypes[1]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -162,7 +162,7 @@ func (x *BadPluginConfig) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use BadPluginConfig.ProtoReflect.Descriptor instead.
 func (*BadPluginConfig) Descriptor() ([]byte, []int) {
-	return file_api_plugins_tests_integration_config_proto_rawDescGZIP(), []int{1}
+	return file_api_tests_integration_config_proto_rawDescGZIP(), []int{1}
 }
 
 func (x *BadPluginConfig) GetPanicInFactory() bool {
@@ -204,7 +204,7 @@ type ConsumerConfig struct {
 func (x *ConsumerConfig) Reset() {
 	*x = ConsumerConfig{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_api_plugins_tests_integration_config_proto_msgTypes[2]
+		mi := &file_api_tests_integration_config_proto_msgTypes[2]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -217,7 +217,7 @@ func (x *ConsumerConfig) String() string {
 func (*ConsumerConfig) ProtoMessage() {}
 
 func (x *ConsumerConfig) ProtoReflect() protoreflect.Message {
-	mi := &file_api_plugins_tests_integration_config_proto_msgTypes[2]
+	mi := &file_api_tests_integration_config_proto_msgTypes[2]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -230,7 +230,7 @@ func (x *ConsumerConfig) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use ConsumerConfig.ProtoReflect.Descriptor instead.
 func (*ConsumerConfig) Descriptor() ([]byte, []int) {
-	return file_api_plugins_tests_integration_config_proto_rawDescGZIP(), []int{2}
+	return file_api_tests_integration_config_proto_rawDescGZIP(), []int{2}
 }
 
 func (x *ConsumerConfig) GetName() string {
@@ -240,13 +240,12 @@ func (x *ConsumerConfig) GetName() string {
 	return ""
 }
 
-var File_api_plugins_tests_integration_config_proto protoreflect.FileDescriptor
+var File_api_tests_integration_config_proto protoreflect.FileDescriptor
 
-var file_api_plugins_tests_integration_config_proto_rawDesc = []byte{
-	0x0a, 0x2a, 0x61, 0x70, 0x69, 0x2f, 0x70, 0x6c, 0x75, 0x67, 0x69, 0x6e, 0x73, 0x2f, 0x74, 0x65,
-	0x73, 0x74, 0x73, 0x2f, 0x69, 0x6e, 0x74, 0x65, 0x67, 0x72, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x2f,
-	0x63, 0x6f, 0x6e, 0x66, 0x69, 0x67, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x1d, 0x61, 0x70,
-	0x69, 0x2e, 0x70, 0x6c, 0x75, 0x67, 0x69, 0x6e, 0x73, 0x2e, 0x74, 0x65, 0x73, 0x74, 0x73, 0x2e,
+var file_api_tests_integration_config_proto_rawDesc = []byte{
+	0x0a, 0x22, 0x61, 0x70, 0x69, 0x2f, 0x74, 0x65, 0x73, 0x74, 0x73, 0x2f, 0x69, 0x6e, 0x74, 0x65,
+	0x67, 0x72, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x2f, 0x63, 0x6f, 0x6e, 0x66, 0x69, 0x67, 0x2e, 0x70,
+	0x72, 0x6f, 0x74, 0x6f, 0x12, 0x15, 0x61, 0x70, 0x69, 0x2e, 0x74, 0x65, 0x73, 0x74, 0x73, 0x2e,
 	0x69, 0x6e, 0x74, 0x65, 0x67, 0x72, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x22, 0x97, 0x01, 0x0a, 0x06,
 	0x43, 0x6f, 0x6e, 0x66, 0x69, 0x67, 0x12, 0x12, 0x0a, 0x04, 0x6e, 0x65, 0x65, 0x64, 0x18, 0x01,
 	0x20, 0x01, 0x28, 0x08, 0x52, 0x04, 0x6e, 0x65, 0x65, 0x64, 0x12, 0x16, 0x0a, 0x06, 0x64, 0x65,
@@ -270,31 +269,31 @@ var file_api_plugins_tests_integration_config_proto_rawDesc = []byte{
 	0x20, 0x01, 0x28, 0x08, 0x52, 0x0b, 0x70, 0x61, 0x6e, 0x69, 0x63, 0x49, 0x6e, 0x49, 0x6e, 0x69,
 	0x74, 0x22, 0x24, 0x0a, 0x0e, 0x43, 0x6f, 0x6e, 0x73, 0x75, 0x6d, 0x65, 0x72, 0x43, 0x6f, 0x6e,
 	0x66, 0x69, 0x67, 0x12, 0x12, 0x0a, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28,
-	0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x42, 0x2c, 0x5a, 0x2a, 0x6d, 0x6f, 0x73, 0x6e, 0x2e,
-	0x69, 0x6f, 0x2f, 0x68, 0x74, 0x6e, 0x6e, 0x2f, 0x61, 0x70, 0x69, 0x2f, 0x70, 0x6c, 0x75, 0x67,
-	0x69, 0x6e, 0x73, 0x2f, 0x74, 0x65, 0x73, 0x74, 0x73, 0x2f, 0x69, 0x6e, 0x74, 0x65, 0x67, 0x72,
-	0x61, 0x74, 0x69, 0x6f, 0x6e, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x42, 0x24, 0x5a, 0x22, 0x6d, 0x6f, 0x73, 0x6e, 0x2e,
+	0x69, 0x6f, 0x2f, 0x68, 0x74, 0x6e, 0x6e, 0x2f, 0x61, 0x70, 0x69, 0x2f, 0x74, 0x65, 0x73, 0x74,
+	0x73, 0x2f, 0x69, 0x6e, 0x74, 0x65, 0x67, 0x72, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x62, 0x06, 0x70,
+	0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
-	file_api_plugins_tests_integration_config_proto_rawDescOnce sync.Once
-	file_api_plugins_tests_integration_config_proto_rawDescData = file_api_plugins_tests_integration_config_proto_rawDesc
+	file_api_tests_integration_config_proto_rawDescOnce sync.Once
+	file_api_tests_integration_config_proto_rawDescData = file_api_tests_integration_config_proto_rawDesc
 )
 
-func file_api_plugins_tests_integration_config_proto_rawDescGZIP() []byte {
-	file_api_plugins_tests_integration_config_proto_rawDescOnce.Do(func() {
-		file_api_plugins_tests_integration_config_proto_rawDescData = protoimpl.X.CompressGZIP(file_api_plugins_tests_integration_config_proto_rawDescData)
+func file_api_tests_integration_config_proto_rawDescGZIP() []byte {
+	file_api_tests_integration_config_proto_rawDescOnce.Do(func() {
+		file_api_tests_integration_config_proto_rawDescData = protoimpl.X.CompressGZIP(file_api_tests_integration_config_proto_rawDescData)
 	})
-	return file_api_plugins_tests_integration_config_proto_rawDescData
+	return file_api_tests_integration_config_proto_rawDescData
 }
 
-var file_api_plugins_tests_integration_config_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
-var file_api_plugins_tests_integration_config_proto_goTypes = []interface{}{
-	(*Config)(nil),          // 0: api.plugins.tests.integration.Config
-	(*BadPluginConfig)(nil), // 1: api.plugins.tests.integration.BadPluginConfig
-	(*ConsumerConfig)(nil),  // 2: api.plugins.tests.integration.ConsumerConfig
+var file_api_tests_integration_config_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
+var file_api_tests_integration_config_proto_goTypes = []interface{}{
+	(*Config)(nil),          // 0: api.tests.integration.Config
+	(*BadPluginConfig)(nil), // 1: api.tests.integration.BadPluginConfig
+	(*ConsumerConfig)(nil),  // 2: api.tests.integration.ConsumerConfig
 }
-var file_api_plugins_tests_integration_config_proto_depIdxs = []int32{
+var file_api_tests_integration_config_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
 	0, // [0:0] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -302,13 +301,13 @@ var file_api_plugins_tests_integration_config_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_api_plugins_tests_integration_config_proto_init() }
-func file_api_plugins_tests_integration_config_proto_init() {
-	if File_api_plugins_tests_integration_config_proto != nil {
+func init() { file_api_tests_integration_config_proto_init() }
+func file_api_tests_integration_config_proto_init() {
+	if File_api_tests_integration_config_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_api_plugins_tests_integration_config_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_api_tests_integration_config_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*Config); i {
 			case 0:
 				return &v.state
@@ -320,7 +319,7 @@ func file_api_plugins_tests_integration_config_proto_init() {
 				return nil
 			}
 		}
-		file_api_plugins_tests_integration_config_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+		file_api_tests_integration_config_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*BadPluginConfig); i {
 			case 0:
 				return &v.state
@@ -332,7 +331,7 @@ func file_api_plugins_tests_integration_config_proto_init() {
 				return nil
 			}
 		}
-		file_api_plugins_tests_integration_config_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
+		file_api_tests_integration_config_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*ConsumerConfig); i {
 			case 0:
 				return &v.state
@@ -349,18 +348,18 @@ func file_api_plugins_tests_integration_config_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_api_plugins_tests_integration_config_proto_rawDesc,
+			RawDescriptor: file_api_tests_integration_config_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   3,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
-		GoTypes:           file_api_plugins_tests_integration_config_proto_goTypes,
-		DependencyIndexes: file_api_plugins_tests_integration_config_proto_depIdxs,
-		MessageInfos:      file_api_plugins_tests_integration_config_proto_msgTypes,
+		GoTypes:           file_api_tests_integration_config_proto_goTypes,
+		DependencyIndexes: file_api_tests_integration_config_proto_depIdxs,
+		MessageInfos:      file_api_tests_integration_config_proto_msgTypes,
 	}.Build()
-	File_api_plugins_tests_integration_config_proto = out.File
-	file_api_plugins_tests_integration_config_proto_rawDesc = nil
-	file_api_plugins_tests_integration_config_proto_goTypes = nil
-	file_api_plugins_tests_integration_config_proto_depIdxs = nil
+	File_api_tests_integration_config_proto = out.File
+	file_api_tests_integration_config_proto_rawDesc = nil
+	file_api_tests_integration_config_proto_goTypes = nil
+	file_api_tests_integration_config_proto_depIdxs = nil
 }
