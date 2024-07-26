mosn/htnn

commit 6109acdea2f10eff86632c54baa3c97573156edf

ci: lint Chinese doc (#641)
Signed-off-by: spacewander <spacewanderlzx@gmail.com>

.github/workflows/website.yml
@@ -18,9 +18,6 @@ jobs:
   check:
     timeout-minutes: 10
     runs-on: ubuntu-latest
-    defaults:
-      run:
-        working-directory: ./site
     steps:
     - uses: actions/checkout@v4
 
@@ -33,7 +30,15 @@ jobs:
           ./site/tmp
         key: ${{ runner.os }}-${{ hashFiles('./Makefile') }}
 
+    - name: lint cjk
+      run: |
+        if ! make lint-cjk; then
+          echo "Please run 'make fix-cjk', edit the detected files and commit changes."
+          exit 1
+        fi
+
     - name: build
+      working-directory: ./site
       run: |
         set -euo pipefail
         make build

Makefile
@@ -64,7 +64,7 @@ dev-tools:
 build-dev-tools:
 	# before running this task, please run `make build-dev-tools-local` and check if the image work locally
 	docker buildx build --platform=linux/amd64,linux/arm64 \
-		--network=host --build-arg GOPROXY=${GOPROXY} -t ${DEV_TOOLS_IMAGE} --push -f tools/Dockerfile.dev ./tools
+		--network=host --build-arg GOPROXY=${GOPROXY} -t ${REAL_DEV_TOOLS_IMAGE} --push -f tools/Dockerfile.dev ./tools
 
 .PHONY: build-dev-tools-local
 build-dev-tools-local:
@@ -159,16 +159,35 @@ lint-editorconfig: $(LOCALBIN)
 	test -x $(LOCALBIN)/editorconfig-checker || GOBIN=$(LOCALBIN) go install github.com/editorconfig-checker/editorconfig-checker/cmd/editorconfig-checker@2.7.2
 	$(LOCALBIN)/editorconfig-checker
 
+.PHONY: lint-cjk
+lint-cjk: dev-tools
+	docker run --rm -v $(PWD):/go/src/${PROJECT_NAME} -w /go/src/${PROJECT_NAME} \
+		${DEV_TOOLS_IMAGE} \
+		autocorrect --lint ./site/content/zh-hans
+
+.PHONY: fix-cjk
+fix-cjk: dev-tools
+	docker run --rm -v $(PWD):/go/src/${PROJECT_NAME} -w /go/src/${PROJECT_NAME} \
+		${DEV_TOOLS_IMAGE} \
+		autocorrect --fix ./site/content/zh-hans
+
+# we don't add this to the umbrella `lint` task because it requires the website to be generated first
+.PHONY: lint-website
+lint-website: $(LOCALBIN)
+	test -x $(LOCALBIN)/htmltest || GOBIN=$(LOCALBIN) go install github.com/wjdp/htmltest@v0.17.0
+	$(LOCALBIN)/htmltest --conf ./.htmltest.yml ./public | grep  -E '(target does not exist|Non-OK status: 404)' \
+		&& exit 1 || true
+
 .PHONY: lint-remain
 lint-remain:
 	grep '>>>>>>' $(shell git ls-files .) | grep -v 'Makefile:' && exit 1 || true
 	go run tools/cmd/linter/main.go
 
 .PHONY: lint
-lint: lint-go lint-proto lint-license lint-spell lint-editorconfig lint-remain
+lint: lint-go lint-proto lint-license lint-spell lint-editorconfig lint-cjk lint-remain
 
 .PHONY: fmt
-fmt: fmt-go fmt-proto
+fmt: fmt-go fmt-proto fix-spell fix-cjk
 
 .PHONY: verify-example
 verify-example:

site/Makefile
@@ -31,9 +31,3 @@ clean:
 publish:
 	# FIXME: change baseURL to real link
 	docker run --rm -v $(PWD):/src --name docsy htnn-docsy -- hugo --minify --gc --baseURL https://mosn.io/
-
-.PHONY: lint-website
-lint-website: $(LOCALBIN)
-	test -x $(LOCALBIN)/htmltest || GOBIN=$(LOCALBIN) go install github.com/wjdp/htmltest@v0.17.0
-	$(LOCALBIN)/htmltest --conf ./.htmltest.yml ./public | grep  -E '(target does not exist|Non-OK status: 404)' \
-		&& exit 1 || true

common.mk
@@ -31,7 +31,8 @@ BUILD_IMAGE     ?= $(DOCKER_MIRROR)docker.io/library/golang:1.21-bullseye
 # We don't use istio/proxyv2 because it is not designed to be run separately (need to work around permission issue).
 PROXY_IMAGE     ?= $(DOCKER_MIRROR)docker.io/envoyproxy/envoy:contrib-v1.29.5
 # We may need to use timestamp if we need to update the image in one PR
-DEV_TOOLS_IMAGE ?= $(DOCKER_MIRROR)ghcr.io/mosn/htnn-dev-tools:2024-03-05
+REAL_DEV_TOOLS_IMAGE ?= ghcr.io/mosn/htnn-dev-tools:2024-07-12
+DEV_TOOLS_IMAGE ?= $(DOCKER_MIRROR)$(REAL_DEV_TOOLS_IMAGE)
 
 ISTIO_VERSION = 1.21.3
 GATEWAY_API_VERSION = 1.0.0

tools/Dockerfile.dev
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-FROM --platform=$TARGETPLATFORM golang:1.21-bullseye
+FROM --platform=$TARGETPLATFORM m.daocloud.io/docker.io/library/golang:1.21-bullseye
 
 # If this image is changed, tell @spacewander to push it.
 # TODO: Maybe we can push this image automatically by tag?
@@ -68,3 +68,11 @@ RUN VER=24.4 && \
 
 # clang-format
 RUN pip install clang-format==17.0.6
+
+# autocorrect
+RUN VER=2.11.1 && \
+    ARCH=$(dpkg --print-architecture) && \
+    wget --progress=dot:giga https://github.com/huacnlee/autocorrect/releases/download/v${VER}/autocorrect-linux-${ARCH}.tar.gz -O autocorrect.tar.gz && \
+    tar -zxvpf autocorrect.tar.gz && \
+    mv autocorrect /usr/bin && \
+    autocorrect --version

commit 1099824671f009dc223e68ecb55b0cc814be6bcc

ci: fix broken website link check
It was broken since https://github.com/mosn/htnn/commit/6109acdea2f10eff86632c54baa3c97573156edf
Signed-off-by: spacewander <spacewanderlzx@gmail.com>

.github/workflows/website.yml
@@ -26,7 +26,6 @@ jobs:
       with:
         # ./tmp is cached for htmltest
         path: |
-          ./site/bin
           ./site/tmp
         key: ${{ runner.os }}-${{ hashFiles('./Makefile') }}
 

.gitignore
@@ -36,4 +36,3 @@ e2e/log
 .idea
 # helm charts
 manifests/charts/**/*.tgz
-.idea

Makefile
@@ -175,8 +175,9 @@ fix-cjk: dev-tools
 .PHONY: lint-website
 lint-website: $(LOCALBIN)
 	test -x $(LOCALBIN)/htmltest || GOBIN=$(LOCALBIN) go install github.com/wjdp/htmltest@v0.17.0
-	$(LOCALBIN)/htmltest --conf ./.htmltest.yml ./public | grep  -E '(target does not exist|Non-OK status: 404)' \
-		&& exit 1 || true
+	cd ./site && $(LOCALBIN)/htmltest --conf ./.htmltest.yml ./public > /tmp/htmltest.log || true
+	@# ignore 'lookup htnn.mosn.io: no such host' error for now
+	test -f /tmp/htmltest.log && (grep -E '(target does not exist|Non-OK status: 404)' /tmp/htmltest.log && exit 1 || true)
 
 .PHONY: lint-remain
 lint-remain: