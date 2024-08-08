473e4b6b5ba9b7b8d6c0b2adf8288b8619e236cd
lyt122 <2747177214@qq.com>
2024-08-08 12:00:56 +0800

    feat: support Consul as ServiceRegistry  (#666)
    ref https://github.com/mosn/htnn/issues/259
    It's the part of refresh the Consul services

--- controller/registries/consul/config_test.go
+++ controller/registries/consul/config_test.go
@@ -15,8 +15,12 @@
 package consul
 
 import (
+	"errors"
+	"reflect"
 	"testing"
 
+	"github.com/agiledragon/gomonkey/v2"
+	"github.com/hashicorp/consul/api"
 	"github.com/stretchr/testify/assert"
 
 	"mosn.io/htnn/controller/pkg/registry/log"
@@ -50,28 +54,42 @@ func TestStart(t *testing.T) {
 		logger: log.NewLogger(&log.RegistryLoggerOptions{
 			Name: "test",
 		}),
-		softDeletedServices: map[consulService]bool{},
-		done:                make(chan struct{}),
-		watchingServices:    map[consulService]bool{},
-	}
-	config := &consul.Config{
-		ServerUrl: "http://127.0.0.1:8500",
+		done: make(chan struct{}),
 	}
 
+	patches := gomonkey.ApplyPrivateMethod(reflect.TypeOf(reg), "fetchAllServices", func(_ *Consul, client *Client) (map[consulService]bool, error) {
+		return map[consulService]bool{
+			{ServiceName: "service1", Tag: "tag1"}: true,
+			{ServiceName: "service2", Tag: "tag2"}: true,
+		}, nil
+	})
+	config := &consul.Config{}
 	err := reg.Start(config)
-	assert.NoError(t, err)
-
+	assert.Nil(t, err)
 	err = reg.subscribe("123")
 	assert.Nil(t, err)
 
 	err = reg.unsubscribe("123")
 	assert.Nil(t, err)
 
-	err = reg.refresh()
-	assert.Nil(t, err)
-
 	err = reg.Stop()
 	assert.Nil(t, err)
+
+	patches.Reset()
+
+	config = &consul.Config{}
+
+	reg = &Consul{
+		logger: log.NewLogger(&log.RegistryLoggerOptions{
+			Name: "test",
+		}),
+		done: make(chan struct{}),
+	}
+
+	err = reg.Start(config)
+	assert.Error(t, err)
+
+	close(reg.done)
 }
 
 func TestReload(t *testing.T) {
@@ -82,4 +100,106 @@ func TestReload(t *testing.T) {
 
 	err := reg.Reload(config)
 	assert.NoError(t, err)
+}
+
+func TestRefresh(t *testing.T) {
+	reg := &Consul{
+		logger: log.NewLogger(&log.RegistryLoggerOptions{
+			Name: "test",
+		}),
+		softDeletedServices: map[consulService]bool{},
+		done:                make(chan struct{}),
+		watchingServices:    map[consulService]bool{},
+	}
+
+	config := &consul.Config{
+		ServerUrl: "http://127.0.0.1:8500",
+	}
+	client, _ := reg.NewClient(config)
+	reg.client = client
+	services := map[string][]string{
+		"service1": {"dc1", "dc2"},
+		"service2": {"dc1"},
+	}
+
+	reg.refresh(services)
+
+	assert.Len(t, reg.watchingServices, 3)
+	assert.Contains(t, reg.watchingServices, consulService{ServiceName: "service1", Tag: "dc1"})
+	assert.Contains(t, reg.watchingServices, consulService{ServiceName: "service1", Tag: "dc2"})
+	assert.Contains(t, reg.watchingServices, consulService{ServiceName: "service2", Tag: "dc1"})
+	assert.Empty(t, reg.softDeletedServices)
+
+	reg = &Consul{
+		logger: log.NewLogger(&log.RegistryLoggerOptions{
+			Name: "test",
+		}),
+		softDeletedServices: map[consulService]bool{},
+		watchingServices: map[consulService]bool{
+			{ServiceName: "service1", Tag: "dc1"}: true,
+		},
+	}
+
+	services = map[string][]string{}
+
+	reg.refresh(services)
+
+	assert.Len(t, reg.watchingServices, 0)
+	assert.Len(t, reg.softDeletedServices, 1)
+
+}
+
+func TestFetchAllServices(t *testing.T) {
+	t.Run("Test fetchAllServices method", func(t *testing.T) {
+		reg := &Consul{
+			logger: log.NewLogger(&log.RegistryLoggerOptions{
+				Name: "test",
+			}),
+		}
+		client := &Client{
+			consulCatalog: &api.Catalog{},
+			DataCenter:    "dc1",
+			NameSpace:     "ns1",
+			Token:         "token",
+		}
+
+		patches := gomonkey.ApplyMethod(reflect.TypeOf(client.consulCatalog), "Services", func(_ *api.Catalog, q *api.QueryOptions) (map[string][]string, *api.QueryMeta, error) {
+			return map[string][]string{
+				"service1": {"tag1", "tag2"},
+				"service2": {"tag3"},
+			}, nil, nil
+		})
+		defer patches.Reset()
+
+		services, err := reg.fetchAllServices(client)
+		assert.NoError(t, err)
+		assert.NotNil(t, services)
+		assert.True(t, services[consulService{ServiceName: "service1", Tag: "tag1"}])
+		assert.True(t, services[consulService{ServiceName: "service1", Tag: "tag2"}])
+		assert.True(t, services[consulService{ServiceName: "service2", Tag: "tag3"}])
+	})
+
+	t.Run("Test fetchAllServices method with error", func(t *testing.T) {
+		reg := &Consul{
+			logger: log.NewLogger(&log.RegistryLoggerOptions{
+				Name: "test",
+			}),
+		}
+		client := &Client{
+			consulCatalog: &api.Catalog{},
+			DataCenter:    "dc1",
+			NameSpace:     "ns1",
+			Token:         "token",
+		}
+
+		patches := gomonkey.ApplyMethod(reflect.TypeOf(client.consulCatalog), "Services", func(_ *api.Catalog, q *api.QueryOptions) (map[string][]string, *api.QueryMeta, error) {
+			return nil, nil, errors.New("mock error")
+		})
+		defer patches.Reset()
+
+		services, err := reg.fetchAllServices(client)
+		assert.Error(t, err)
+		assert.Equal(t, "mock error", err.Error())
+		assert.Nil(t, services)
+	})
 }
