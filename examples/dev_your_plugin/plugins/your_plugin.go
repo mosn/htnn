157dda0b79292f291f93588d158746521ab9686f
Jianbo Sun <jianbosun@microsoft.com>
2024-09-25 18:01:24 +0800

    refine the example with full interface

--- examples/dev_your_plugin/plugins/your_plugin.go
+++ examples/dev_your_plugin/plugins/your_plugin.go
@@ -15,7 +15,9 @@
 package plugins
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 
 	"mosn.io/htnn/api/pkg/filtermanager/api"
 	"mosn.io/htnn/api/pkg/plugins"
@@ -58,9 +60,44 @@ type filter struct {
 func (f *filter) DecodeHeaders(headers api.RequestHeaderMap, endStream bool) api.ResultAction {
 	hdr := http.Header{}
 	hdr.Set("content-type", "text/plain")
+	allHeaders := headers.GetAllHeaders()
+	msg := "Headers:\n"
+	for k, v := range allHeaders {
+		msg += k + ": " + strings.Join(v, ",") + "\n"
+	}
 	return &api.LocalResponse{
 		Code:   200,
-		Msg:    "Your plugin is run\n",
+		Msg:    "Your plugin is running\n" + msg,
 		Header: hdr,
 	}
+}
+
+// DecodeData might be called multiple times during handling the request body.
+// The endStream is true when handling the last piece of the body.
+func (f *filter) DecodeData(data api.BufferInstance, endStream bool) api.ResultAction {
+	if !endStream {
+		return api.WaitAllData
+	}
+	requestBody := data.Bytes()
+	fmt.Println("Request body: ", string(requestBody))
+	return api.Continue
+}
+
+// EncodeHeaders processes response headers. The endStream is true if the response doesn't have body
+func (f *filter) EncodeHeaders(headers api.ResponseHeaderMap, endStream bool) api.ResultAction {
+	headers.Add("my-plugin", "running")
+	return api.Continue
+}
+
+// EncodeData might be called multiple times during handling the response body.
+// The endStream is true when handling the last piece of the body.
+func (f *filter) EncodeData(data api.BufferInstance, endStream bool) api.ResultAction {
+	return api.Continue
+
+}
+
+// OnLog is called when the HTTP stream is ended on HTTP Connection Manager filter.
+func (f *filter) OnLog(reqHeaders api.RequestHeaderMap, reqTrailers api.RequestTrailerMap,
+	respHeaders api.ResponseHeaderMap, respTrailers api.ResponseTrailerMap) {
+	fmt.Println("this is my plugin log")
 }
